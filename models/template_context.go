cbe8c31010c73b491bea79a6cc72eece3c3f432a
GgBoom-993 <55505202+GgBoom-993@users.noreply.github.com>
2024-01-30 10:27:38 +0800

    Update template_context.go

--- models/template_context.go
+++ models/template_context.go
@@ -74,13 +74,14 @@ func NewPhishingTemplateContext(ctx TemplateContext, r BaseRecipient, rid string
   trackingURL.Path = path.Join(trackingURL.Path, "/track")
   trackingURL.RawQuery = q.Encode()
   
-  //这里x.x.x.x换成和钓鱼邮箱域名绑定的ip地址
+
   ipMap := map[string]string{
     "x.x.x.x": "www.example.com",
   }
-  // 调用替换函数
+
   newURL := replaceIPWithDomain(phishURL.String(), ipMap)  
 
+  // If QR logo needed, plz replace the value of the parameter logoUrl 
   apiURL := "http://qrcode.hlcode.cn/beautify/style/create?bgColor=%23FFFFFF&bodyType=1&content=" + newURL + "&down=0&embedPosition=0&embedText=&embedTextColor=%23000000&embedTextSize=38&eyeInColor=%23000000&eyeOutColor=%23000000&eyeType=8&eyeUseFore=1&fontFamily=0&foreColor=%23000000&foreColorImage=&foreColorTwo=&foreType=0&frameColor=&gradientWay=0&level=H&logoShadow=0&logoShap=1&logoUrl=https:%2F%2Foss.hlcode.cn%2Fserver%2F2024%2F01%2F04%2F164890235818.png&margin=2&rotate=30&size=400&qrCodeId=0&format=1"
   
   imageURL, err := GetQRCodeImageURL(apiURL)
@@ -98,30 +99,30 @@ func NewPhishingTemplateContext(ctx TemplateContext, r BaseRecipient, rid string
 }
 
 func convertImageToBase64(url string) (string, error) {
-  // 获取远程图片的内容
+ 
   imageContent, err := fetchRemoteImage(url)
   if err != nil {
     return "", err
   }
 
-  // 将图片内容转换为Base64编码
+
   base64Image := base64.StdEncoding.EncodeToString(imageContent)
 
-  // 构造Base64格式的图片URL
+
   base64DataURL := "data:image/png;base64," + base64Image
 
   return base64DataURL, nil
 }
 
 func fetchRemoteImage(url string) ([]byte, error) {
-  // 发起HTTP请求获取远程图片内容
+
   response, err := http.Get(url)
   if err != nil {
     return nil, err
   }
   defer response.Body.Close()
 
-  // 读取图片内容
+
   imageContent, err := ioutil.ReadAll(response.Body)
   if err != nil {
     return nil, err
@@ -132,38 +133,38 @@ func fetchRemoteImage(url string) ([]byte, error) {
 
 func replaceIPWithDomain(url string, ipMap map[string]string) string {
   for ip, domain := range ipMap {
-    // 将IP地址替换为域名
+
     url = strings.Replace(url, ip, domain, -1)
   }
   return url
 }
 
 func GetQRCodeImageURL(apiURL string) (string, error) {
-  // 发送GET请求
+
   response, err := http.Get(apiURL)
   if err != nil {
     return "", fmt.Errorf("Error making GET request: %v", err)
   }
   defer response.Body.Close()
 
-  // 检查响应是否成功 (状态码为 200-299)
+
   if response.StatusCode < 200 || response.StatusCode >= 300 {
     return "", fmt.Errorf("Error: Unexpected status code %s", response.Status)
   }
 
-  // 解析JSON响应
+
   var apiResponse ApiResponse
   err = json.NewDecoder(response.Body).Decode(&apiResponse)
   if err != nil {
     return "", fmt.Errorf("Error decoding JSON: %v", err)
   }
 
-  // 检查API响应中的错误码
+
   if apiResponse.Code != 0 {
     return "", fmt.Errorf("API returned an error: %s", apiResponse.Msg)
   }
 
-  // 返回图片地址
+
   return apiResponse.Data, nil
 }
 
